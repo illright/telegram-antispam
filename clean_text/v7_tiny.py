9a8e1e2bc370b357ed2f321c6f2ed7d9bca2c83c
Lev Chelyadinov <leva181777@gmail.com>
2025-01-16 21:03:30 +0100

    Capitalize sentences before checking

--- clean_text/v7_tiny.py
+++ clean_text/v7_tiny.py
@@ -6,4 +6,7 @@ import re
 def clean_text(text: str):
     text = re.sub(r"http\S+", "", text)
     text = re.sub(r"@[a-zA-Z0-9_]{5,}", "", text)
-    return text
+    return capitalize_sentences(text)
+
+def capitalize_sentences(text: str):
+    return "".join(part.capitalize() for part in re.split(r"([\.!?]\s+)", text))
